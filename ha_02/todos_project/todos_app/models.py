1c728c96e786f314fb3f6242e841736621953fed
Deniz Furater <24463053+pozilei@users.noreply.github.com>
2020-05-22 18:14:00 +0200

    implemented django config

--- ha_02/todos_project/todos_app/models.py
+++ ha_02/todos_project/todos_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    description = models.CharField(max_length=160)    
+    due = models.CharField(max_length=80)
+    accomplished = models.IntegerField()
+
+    def __str__(self):
+        return self.description
