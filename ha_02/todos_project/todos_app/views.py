d041786ad44ef42711a8b76897d81f874f21d38f
Aziz Misir <aziz.misir@campus.tu-berlin.de>
2020-05-22 22:12:10 +0200

    add function for views urls

--- ha_02/todos_project/todos_app/views.py
+++ ha_02/todos_project/todos_app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from todos_app.models import Author, Todo
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+
+
+def index(request):
+    todo_list = Todo.objects.all()
+    context = {
+        'todo_list': todo_list,
+    }
+    return render(request, 'todos_app/index.html', context=context)
+
+
+def impressum(request):
+    author_list = Author.objects.all()
+    context = {
+        'author_list': author_list,
+    }
+    return render(request, 'todos_app/impressum.html', context=context)
+
+
+def remove(request, pk):
+    elm = Todo.objects.get(pk=pk)
+    elm.delete()
+    return HttpResponseRedirect('/todos_app/')
